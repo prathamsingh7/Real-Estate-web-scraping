a15b55b414e15988291f7d279864eeefc1285035
Pratham Singh <78151566+prathamsingh7@users.noreply.github.com>
2023-09-19 20:02:53 +0530

    Add files via upload

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from googletrans import Translator
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import pandas as pd
-import psycopg2
 import time
 
 options = webdriver.ChromeOptions()
@@ -24,7 +20,7 @@ time.sleep(5)
 # district_path = "/html/body/div[1]/div[1]/main/div[1]/div/form/div[2]/div[2]/div[1]/select/option[26]"
 # taluka_path = "/html/body/div[1]/div[1]/main/div[1]/div/form/div[2]/div[2]/div[2]/select/option[2]"
 # village_path = "/html/body/div[1]/div[1]/main/div[1]/div/form/div[2]/div[2]/div[3]/select/option[59]"
-# reg_reg_year_path = "/html/body/div[1]/div[1]/main/div[1]/div/form/div[2]/div[1]/div[2]/select/option[31]"
+# year_path = "/html/body/div[1]/div[1]/main/div[1]/div/form/div[2]/div[1]/div[2]/select/option[31]"
 # entry_path = "/html/body/div[1]/div[1]/main/div[2]/div/div/div[2]/div/div[1]/div[1]/div/label/select/option[3]"
 
 # def select_district():
@@ -43,10 +39,10 @@ time.sleep(5)
 #     driver.find_element(By.XPATH, village_path).click()
 #     time.sleep(2)
     
-# def select_reg_reg_year():
+# def select_year():
 #     driver.find_element(By.ID, "dbselect").click()
 #     # drop.select_by_index(30)
-#     driver.find_element(By.XPATH, reg_reg_year_path).click()
+#     driver.find_element(By.XPATH, year_path).click()
 #     time.sleep(2)
 
 # def entry_number():
@@ -54,13 +50,13 @@ time.sleep(5)
 #     driver.find_element(By.XPATH, entry_path).click()
 #     time.sleep(2)
     
-# def reg_reg_reg_year():
-#     type_reg_reg_year = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div[1]/div/form/div[3]/div[2]/input")
-#     type_reg_reg_year.send_keys("2023")
+# def reg_year():
+#     type_year = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div[1]/div/form/div[3]/div[2]/input")
+#     type_year.send_keys("2023")
 #     time.sleep(2)
     
 # Defining all the required functions using Select() and select_by_value()
-def select_reg_reg_year():
+def select_year():
     table1 = driver.find_element(By.ID, "dbselect")
     drop_table = Select(table1)
     drop_table.select_by_index(30)
@@ -84,7 +80,7 @@ def select_village():
     drop_table.select_by_value(str(57))
     time.sleep(2)
     
-def reg_reg_reg_year():
+def reg_year():
     # table5 = driver.find_element({"class" : "form-control", "id" : "free_text"})
     table5 = driver.find_element(By.CSS_SELECTOR, ".form-control#free_text")
     table5.send_keys("2023")
@@ -97,12 +93,12 @@ def entry_number():
     drop_table.select_by_value(str(50))
     time.sleep(2)
 
-# Select the reg_reg_year, district, taluka, village and reg reg_reg_year
-select_reg_reg_year()  
+# Select the year, district, taluka, village and reg_year
+select_year()  
 select_district() 
 select_taluka()
 select_village()
-reg_reg_reg_year()
+reg_year()
 
 # Manually entering captcha
 
@@ -143,65 +139,3 @@ for row in rows:
 
 df  =  pd.DataFrame({'Serial number': serial_number, 'Document number': document_number, 'Document type' : document_type, 'Revenue Office': revenue_office, 'Registration Year': reg_year, 'Buyer name': Buyer_name, 'Seller name': Seller_name, 'Other information' : Other_information, 'List no.2': List_no_2})
 df.to_csv('data.csv', index = False)
-
-# translator = Translator()
-
-# translated_document_type = []
-# translated_revenue_office = []
-# translated_Buyer_name = []
-# translated_Seller_name = []
-# translated_Other_information = []
-
-# for text in document_type:
-#     if text.strip():
-#         try:
-#             translation = translator.translate(text, src='hi', dest='en')
-#             translated_document_type.append(translation.text)
-#         except:
-#             translated_document_type.append("")
-#     else:
-#         translated_document_type.append("")
-
-# for text in revenue_office:
-#     if text.strip():
-#         try:
-#             translation = translator.translate(text, src='hi', dest='en')
-#             translated_revenue_office.append(translation.text)
-#         except:
-#             translated_revenue_office.append("")
-#     else:
-#         translated_revenue_office.append("")
-
-# for text in Buyer_name:
-#     if text.strip():
-#         try:
-#             translation = translator.translate(text, src='hi', dest='en')
-#             translated_Buyer_name.append(translation.text)
-#         except:
-#             translated_Buyer_name.append("")
-#     else:
-#         translated_Buyer_name.append("")
-
-# for text in Seller_name:
-#     if text.strip():
-#         try:
-#             translation = translator.translate(text, src='hi', dest='en')
-#             translated_Seller_name.append(translation.text)
-#         except:
-#             translated_Seller_name.append("")
-#     else:
-#         translated_Seller_name.append("")
-
-# for text in Other_information:
-#     if text.strip():
-#         try:
-#             translation = translator.translate(text, src='hi', dest='en')
-#             translated_Other_information.append(translation.text)
-#         except:
-#             translated_Other_information.append("")
-#     else:
-#         translated_Other_information.append("")
-
-# df  =  pd.DataFrame({'Serial number': serial_number, 'Document number': document_number, 'Document type' : document_type, 'Revenue Office': revenue_office, 'Registration Year': reg_year, 'Buyer name': Buyer_name, 'Seller name': Seller_name, 'Other information' : Other_information, 'List no.2': List_no_2})
-# df.to_csv('data.csv', index = False)
-# df  = pd.read_csv('data.csv')
